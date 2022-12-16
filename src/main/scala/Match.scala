01ba474df1bbf8bc8e23f986dc2f0140ccfa0bb5
rizamittal <er.riza230188@gmail.com>
2022-12-16 23:32:05 +0000

    Ifelse

--- src/main/scala/Match.scala
+++ src/main/scala/Match.scala
@@ -1,11 +1,19 @@
 object Match {
-   def main(args: Array[String]): Unit = {
-     val i: Int  = 1
-     val result = i match {
-       case 1 => "one"
-       case 2 => "two"
-       case _ => "not 1 or 2"
-     }
-print(result)
-   }
+  def main(args: Array[String]): Unit = {
+    val i: Int = 1
+    val result = i match {
+      case 1 => "one"
+      case 2 => "two"
+      case _ => "not 1 or 2"
+    }
+    println(result)
+
+    val bool = true
+    val booleanAsString = bool match {
+      case true => "true"
+      case false => "false"
+    }
+    println(booleanAsString)
+
+  }
 }
