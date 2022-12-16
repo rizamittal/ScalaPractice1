7149e107f587e11a6e9a97648af58f50710836b4
rizamittal <er.riza230188@gmail.com>
2022-12-16 10:26:39 +0000

    my first program

---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,10 @@
+object Hello {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+    val a=10
+    val b= 20
+    val c= a+b
+    print(c)
+  }
+}
---
+++ src/main/scala/Hellow_world.scala
@@ -0,0 +1,8 @@
+object Hellow_world extends App {
+  println("Hello, world")
+  val a = 10
+  val b = 20
+  val c = a + b
+  print(c)
+  print("heloo2")
+}
