49f1421a90558c4303d40e80ea5c036785e0b7a3
rizamittal <er.riza230188@gmail.com>
2022-12-16 23:02:33 +0000

    Ifelse

---
+++ src/main/scala/if_else.scala
@@ -0,0 +1,11 @@
+object if_else {
+  def main(args: Array[String]) : Unit = {
+    val a = 10
+    val b = 20
+   val x =  if (a<b)
+               print(a)
+            else
+               print(b)
+  }
+
+}
